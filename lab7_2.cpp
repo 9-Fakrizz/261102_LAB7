2d66c89076b01ebe230e201e9402ccadd0454e9a
suphakrit <suphakrit_kongkham@cmu.ac.th>
2022-12-13 15:52:19 +0700

    Lab7_2 Success

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,10 +4,29 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+	int i = 0, P[5000], k, result=0, j=0;
+	while(j < 5000){
+		P[j] = (2*(j+1))-1;
+		j++;
+	}
+	cout << "Please input k: ";
+	cin >> k;
+	if(k>0){
+		while(i < 5000){
+			if(P[i]%k==0){
+				result = result - P[i];
+				i++;
+			}else{
+				result = result + P[i];
+				i++;
+			}
+			
+		}
+		cout << "Result = "<< result;
+	
+	}
+	else{
+		cout<<"Invalid input!!!";
 	}
 	
 	return 0;
